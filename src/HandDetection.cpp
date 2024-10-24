7815b72e3f90533f9b9c282969d0a579f8d55a3e
LordOfBon <90770043+LordOfBon@users.noreply.github.com>
2024-10-23 23:30:13 -0230

    Fixes to imgui display

--- src/HandDetection.cpp
+++ src/HandDetection.cpp
@@ -1,9 +1,5 @@
 #include "HandDetection.h"
 
-#include <SFML/Graphics.hpp>
-#include "imgui.h"
-#include "imgui-SFML.h"
-
 void HandDetection::imgui(const cv::Mat & input)
 {
     try
@@ -15,31 +11,28 @@ void HandDetection::imgui(const cv::Mat & input)
         cv::Canny(adjusted, adjusted, m_thresh, m_thresh * 2);
 
         std::vector<std::vector<cv::Point>> contours;
-        cv::findContours(adjusted, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+        cv::findContours(adjusted, contours, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
 
-        std::vector<std::vector<cv::Point> >hull(contours.size());
+        std::vector<std::vector<cv::Point>> hull(contours.size());
+        //ImGui::Text("Contours: %d", contours.size());
         for (size_t i = 0; i < contours.size(); i++)
         {
             cv::convexHull(contours[i], hull[i]);
+            //ImGui::Text("   C%d: %d", i, contours[i].size());
         }
-
-        ImGui::Text("Contours: %d", contours.size());
-
-        cv::RNG rng(68);
-        cv::Mat drawing = cv::Mat::zeros(adjusted.size(), CV_8UC3);
+        cv::Mat drawing = cv::Mat::zeros(adjusted.size(), CV_8UC4) + cv::Scalar(0,0,0,255);
         for (size_t i = 0; i < contours.size(); i++)
         {
-            cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-            cv::drawContours(drawing, contours, (int)i, color);
-            cv::drawContours(drawing, hull, (int)i, color);
+            cv::Scalar color = cv::Scalar(255, 0, 0, 255);
+            //cv::drawContours(drawing, contours, (int)i, color, -1);
+            cv::drawContours(drawing, hull, (int)i, color, -1);
         }
-        cv::cvtColor(drawing, drawing, cv::COLOR_BGR2RGBA);
 
-        sf::Image image;
+        //cv::connectedComponents(drawing, drawing);
+
         image.create(drawing.cols, drawing.rows, drawing.ptr());
-        sf::Texture tex;
         tex.loadFromImage(image);
-        ImGui::Image(tex);
+        ImGui::Image(tex, sf::Color::White);
     }
     catch (cv::Exception e)
     {
--- src/HandDetection.h
+++ src/HandDetection.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
+#include <SFML/Graphics.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
 
 class HandDetection
 {
     int m_thresh = 100;
+    sf::Image image;
+    sf::Texture tex;
 public:
     void imgui(const cv::Mat & input);
 };
