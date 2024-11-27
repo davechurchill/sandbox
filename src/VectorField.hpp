f1213a1b92baa95d28e8d57fdfb4f44b72e85f1a
Ethan Denny <ethandenny@protonmail.com>
2024-11-27 18:55:56 -0330

    Revert "BFS experiment"
    This reverts commit 9a5cd573968c373c5fedf76e06ccb562b6ddef30.

--- src/VectorField.hpp
+++ src/VectorField.hpp
@@ -8,7 +8,7 @@
 
 namespace VectorField
 {
-    Grid<sf::Vector2<float>> compute(const cv::Mat& grid, int spacing, float terrainWeight)
+    Grid<sf::Vector2<double>> compute(const cv::Mat& grid, int spacing, float terrainWeight)
     {
         int pixelWidth = grid.cols;
         int pixelHeight = grid.rows;
@@ -23,16 +23,16 @@ namespace VectorField
             gridHeight += 1;
         }
 
-        Grid<float> m_cost = Grid<float>(gridWidth + 1, gridHeight, 0); // average terrain height
-        Grid<float> m_bestCost = Grid<float>(gridWidth + 1, gridHeight, std::numeric_limits<float>::max());
-        Grid<sf::Vector2<float>> m_directions = Grid<sf::Vector2<float>>(gridWidth, gridHeight, { 0, 0 }); // director vector
+        Grid<double> m_grid = Grid<double>(gridWidth, gridHeight, 0); // average terrain height
+        Grid<double> m_distance = Grid<double>(gridWidth + 1, gridHeight, -1); // distance to nearest goal cell * (1 + terrain height * weight) 
+        Grid<sf::Vector2<double>> m_directions = Grid<sf::Vector2<double>>(gridWidth, gridHeight, { 0, 0 }); // director vector
 
         for (int x = 0; x < gridWidth; ++x)
         {
             for (int y = 0; y < gridHeight; ++y)
             {
                 int count = 0;
-                float sum = 0;
+                double sum = 0;
 
                 for (int sx = 0; sx < spacing && x * spacing + sx < pixelWidth; ++sx)
                 {
@@ -43,7 +43,7 @@ namespace VectorField
                     }
                 }
 
-                m_cost.set(x, y, sum / count);
+                m_grid.set(x, y, sum / count);
             }
         }
 
@@ -52,7 +52,7 @@ namespace VectorField
         for (int y = 0; y < gridHeight; ++y)
         {
             openList.push({ gridWidth, y });
-            m_bestCost.set(gridWidth, y, 0);
+            m_distance.set(gridWidth, y, 0);
         }
 
         while (!openList.empty())
@@ -60,17 +60,15 @@ namespace VectorField
             sf::Vector2i cell = openList.front();
             openList.pop();
 
-            auto currBestCost = m_bestCost.get(cell.x, cell.y);;
+            auto value = m_distance.get(cell.x, cell.y);
 
             auto update = [&](int x, int y)
             {
-                float& neighbourBestCost = m_bestCost.get(x, y);
-                float newCost = currBestCost + (1 - terrainWeight) + m_cost.get(x, y) * terrainWeight;
-                
-                if (newCost < neighbourBestCost)
+                auto& dist = m_distance.get(x, y);
+                if (dist == -1)
                 {
                     openList.push({ x, y });
-                    neighbourBestCost = newCost;
+                    dist = value + 1;
                 }
             };
 
@@ -96,16 +94,24 @@ namespace VectorField
         {
             for (int y = 0; y < gridHeight; ++y)
             {
-                auto bestCost = m_bestCost.get(x, y);
+                m_distance.get(x, y) *= 1 + m_grid.get(x, y) * terrainWeight;
+            }
+        }
 
-                float left = x > 0 ? m_bestCost.get(x - 1, y) : bestCost;
-                float right = x < gridWidth - 1 ? m_bestCost.get(x + 1, y) : bestCost;
-                float up = y > 0 ? m_bestCost.get(x, y - 1) : bestCost;
-                float down = y < gridHeight - 1 ? m_bestCost.get(x, y + 1) : bestCost;
+        for (int x = 0; x < gridWidth; ++x)
+        {
+            for (int y = 0; y < gridHeight; ++y)
+            {
+                auto thisDist = m_distance.get(x, y);
 
-                sf::Vector2<float> dir{ left - right, up - down };
+                double left = x > 0 ? m_distance.get(x - 1, y) : thisDist;
+                double right = x < gridWidth - 1 ? m_distance.get(x + 1, y) : thisDist;
+                double up = y > 0 ? m_distance.get(x, y - 1) : thisDist;
+                double down = y < gridHeight - 1 ? m_distance.get(x, y + 1) : thisDist;
 
-                float magnitude = std::sqrt(dir.x * dir.x + dir.y * dir.y);
+                sf::Vector2<double> dir{ left - right, up - down };
+
+                double magnitude = std::sqrt(dir.x * dir.x + dir.y * dir.y);
 
                 m_directions.get(x, y) = { dir.x / magnitude, dir.y / magnitude };
             }
